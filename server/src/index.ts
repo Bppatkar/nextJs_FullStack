2bfe1780cf33ae35e967fbd78a42db41855fa889
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-12-05 19:24:11 +0530

    vies update with login sign up forgot page done

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,16 +3,23 @@ import type { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const _dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+
+//  setting View engine ejs
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(_dirname, './views'));
 
 app.get('/', (req: Request, res: Response) => {
-  return res.send('Hey Its working');
+  return res.render('welcome');
 });
 
 app.listen(PORT, () => {
