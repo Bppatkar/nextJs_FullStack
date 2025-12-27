36e79aec08d875b3afb84824626d51878ec2ba6e
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-12-27 20:49:15 +0530

    fixing minor changes

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,18 +1,14 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { fontsans } from 'next/font/google';
 import './globals.css';
 
 import { Toaster } from 'sonner';
 import ClientSessionProvider from './providers/ClientSessionProvider';
+import { cn } from '@/lib/utils';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const fontSans = fontSans({
   subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: '--font-sans',
 });
 
 export const metadata: Metadata = {
@@ -28,7 +24,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          'min-h-screen  font-sans antialiased bg-slate-50',
+          fontSans.variable
+        )}
       >
         <ClientSessionProvider>
           {children}
